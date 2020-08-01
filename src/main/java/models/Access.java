e134b85b0aeaa54fda7eac560a2d5e521415b0df
Guiller Dalit <gelierdalit@gmail.com>
2020-07-31 18:36:05 -0700

    Added two models for encapsulation Follower, Access

---
+++ src/main/java/models/Access.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Access {
+
+    private int account_id = -1;
+    private int recipe_id = -1;
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getRecipe_id() {
+        return recipe_id;
+    }
+
+    public void setRecipe_id(int recipe_id) {
+        this.recipe_id = recipe_id;
+    }
+}
